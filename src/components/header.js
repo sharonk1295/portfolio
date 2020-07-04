14df8f6f5b91fc293bcef6fa0c87a7da7b147a4c
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-07-03 20:03:27 -0400

    added media query

--- src/components/header.js
+++ src/components/header.js
@@ -2,44 +2,22 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import headerStyles from "./header.module.css"
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `linear-gradient(0deg, rgba(248,249,251,0) 0%, rgba(228,195,209,1) 95%)`,
-      // marginBottom: `0.4rem`,
-      textAlign: `center`
-    }}
+    className={headerStyles.header}
   >
     <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `2.5rem 1.0875rem`,
-      }}
+      className={headerStyles.headerText}
     >
-      <h1 className="name-title" style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textShadow: `whitesmoke 2px 1px`,
-            textDecoration: `none`,
-            fontSize: `100px`,
-            letterSpacing: `1px`,
-            fontFamily: `Playfair Display SC, serif`,
-          }}
-        >
+      <h1 className={headerStyles.title}>
+        <Link to="/">
           {siteTitle}
         </Link>
       </h1>
-      <p style={{
-        color: `gray`,
-        fontWeight: `600`,
-        fontFamily: `Abel, sans-serif`,
-      }}>Software Engineer <span style={{
-        color: `purple`
-      }}>|</span> Full-Stack Developer</p>
+      <p className={headerStyles.description}>
+        Software Engineer <span>|</span> Full-Stack Developer</p>
     </div>
   </header>
 )

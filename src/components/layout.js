e88b5d31b607b812a946529166094ddbfb525508
Sharon Kim <carolinek@SKs-MacBook-Air.local>
2020-07-04 00:34:35 -0400

    installed gatsby bg-img and added styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,7 +32,8 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          // maxWidth: 960,
+          // padding: `0 1.0875rem 1.45rem`,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,49 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
+import { graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
-//import Image from "../components/image"
 import SEO from "../components/seo"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BackgroundImage from "gatsby-background-image"
+import indexStyles from "./index.module.css"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 style={{
-      fontFamily: `Barlow Semi Condensed, sans-serif`,
-      letterSpacing: `1px`
-    }}>Hi Sharon</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    indexPic: file(relativePath: { eq: "profile_photo.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1200, maxHeight: 1200, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`)
+
+  return (
+    <BackgroundImage fluid={data.indexPic.childImageSharp.fluid} fadeIn={true}>
+      <Layout>
+        <SEO title="Home" />
+          <div className={indexStyles.background}>
+            <h1>Hi, I'm Sharon Kim.</h1>
+            <p>A full-stack developer with a creative passion <br/>for building apps to improve user experience.</p>
+            <button><Link to="/about">Learn More</Link></button>
+          </div>
+      </Layout>
+    </BackgroundImage>
+  )}
+
 
 export default IndexPage
+
+// {/* <Layout>
+//     <SEO title="Home" />
+//     <BackgroundImage fluid={data.indexPic.childImageSharp.fluid} fadeIn={true}>
+//       <div className={indexStyles.background}>
+//         <h1>Hi, I'm Sharon Kim.</h1>
+//         <p>A full-stack developer with a creative passion <br/>for building apps to improve user experience.</p>
+//         <button><Link to="/about">Learn More</Link></button>
+//       </div>
+//     </BackgroundImage>
+//   </Layout> */}
